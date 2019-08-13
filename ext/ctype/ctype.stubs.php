7dc1a5ee760f9be525e8d8eaa9ae66d6f5b1dc9e
Victor Gazotti <victor@victor-note.victor>
2019-08-13 20:01:42 -0300

    ctype stubs

---
+++ ext/ctype/ctype.stubs.php
@@ -0,0 +1,23 @@
+<?php
+
+function ctype_alnum(string $text): bool {}
+
+function ctype_alpha(string $text): bool {}
+
+function ctype_cntrl(string $text): bool {}
+
+function ctype_digit(string $text): bool {}
+
+function ctype_lower(string $text): bool {}
+
+function ctype_graph(string $text): bool {}
+
+function ctype_print(string $text): bool {}
+
+function ctype_punct(string $text): bool {}
+
+function ctype_space(string $text): bool {}
+
+function ctype_upper(string $text): bool {}
+
+function ctype_xdigit(string $text): bool {}
